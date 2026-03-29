ae1976a97cc5e65dadaae5ac59466fdfb35f371f
Carlos Jimenez H. <cjhirashi@gmail.com>
2026-03-29 15:25:37 -0600

    Add initial project structure and templates for INNES testing lab

---
+++ home/views.py
@@ -0,0 +1,11 @@
+# core/views.py
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+	return render(request, "home/home.html")
+
+
+def health(request):
+	return HttpResponse("OK")
